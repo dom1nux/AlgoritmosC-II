867f29657dd7c24e0c76b1ec6e7116a95a462263
Thony-KB00M <anthonydiazbecerra4@gmail.com>
2024-01-03 22:03:06 -0500

    ingresar elementos de un array

--- Arreglos/src/Main.java
+++ Arreglos/src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner Ss = new Scanner(System.in);
+
+        System.out.println("Ingrese el número de datos: ");
+        int N = Ss.nextInt();
+        int [] Arr = new int[N];
+
+        for (int a = 0; a < N; a++){
+            System.out.println("Ingrese un digito: ");
+            int F = Ss.nextInt();
+            Arr[a]=F;
+        }
+
+        for (int b = 0; b < N; b++){
+            int K= b;
+            for(int c = b+1; c < N; c++){
+                if (Arr[c]<Arr[K]){
+                    K=c;
+                }
+            }
+            int G = Arr[b];
+            Arr[b] = Arr[K];
+            Arr[K] = G;
+        }
+        for (int d = 0; d < N; d++){
+            System.out.println(Arr[d]);
+        }
     }
-}
+
+
+
+    }
