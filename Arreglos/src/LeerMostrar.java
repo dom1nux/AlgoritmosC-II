39d9b6431790072c657aef8b4a085195054ef71d
Thony-KB00M <anthonydiazbecerra4@gmail.com>
2024-01-03 22:06:41 -0500

    LeerMostrarArray

---
+++ Arreglos/src/LeerMostrar.java
@@ -0,0 +1,14 @@
+public class LeerMostrar {
+    public static void main(String[] args) {
+        int[] LeerMostrar = new int[10];
+        for (int i = 0; i < 10; i++) {
+            LeerMostrar[i] = i + 1;
+        }
+
+        for (int x = 0; x < 10; x++) {
+            System.out.println(LeerMostrar[x] + "");
+        }
+    }
+}
+
+
